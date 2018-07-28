7eb4126b3c8f0050ee3302afd6759dd1bc9bdb8e
github username <github email address>
2018-07-28 11:29:44 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+    a = side1 + side2
+    b = side1 + side3
+    c = side2 + side3
+  end
+    
+  def kind
+    if a > side3 && b > side2 && c > side1
+      if a = b && a = c && b = c
+        :equilateral
+      elsif (a = b && a != c) || (a = c && a != b) || (b = c && b != a)
+        :isosceles
+      elsif a != b && a != c && b != c
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      rescue TriangleError => exception
+        puts exception.message
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def messsage
+      "The triangle in sot valid."
+    end
+  end
 end
